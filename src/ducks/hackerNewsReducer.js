dd405c56525e1dc30155aed244fcbe4987b443f7
Benjamin Doggett <bbdoggett3@gmail.com>
2020-06-01 23:49:59 -0600

    create part 2

---
+++ src/ducks/hackerNewsReducer.js
@@ -0,0 +1,8 @@
+let initialState = {
+    loading: false,
+    articles: []
+}
+
+export default function reducer(state = initialState, action) {
+    return state;
+}
---
+++ src/store.js
@@ -0,0 +1,4 @@
+import {createStore} from 'redux'
+import hackerNewsReducer from '../src/ducks/hackerNewsReducer'
+
+export default createStore(hackerNewsReducer);
